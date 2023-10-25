de5680b1eb634fca745cfc0e5fccc70867a65b8f
Samba <theodorebinda@gmail.com>
2023-10-25 21:37:56 +0100

    function qui prend le dernier element

--- .vscode/index3.js
+++ .vscode/index3.js
@@ -19,7 +19,7 @@
 //     }
 
 // }
-getFistElemArr(["merci",3,5,6])
+// getFistElemArr(["merci",3,5,6])
 // Ex3 Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
 function getFirstElements(array, n) {
     if (n === undefined) {
@@ -28,8 +28,8 @@ function getFirstElements(array, n) {
       return array.slice(0, n);
     }
   }
-  const myArray = [1, 2, 3, 4, 5];
-const firstElement = getFirstElements(myArray); // renvoie 1
-const firstTwoElements = getFirstElements(myArray, 2); // renvoie [1, 2]
+  const myArray = [1, 2, 3, 4];
+const firstElement = getFirstElements(myArray); 
+const firstTwoElements = getFirstElements(myArray, 3);
 
   
