168b6cf2aa37ca53ff94832c470b4c5df23396c2
Théodorebinda <theodorebinda@gmail.com>
2023-10-25 20:08:48 +0100

    fonction qui prend le n element du tableau

--- .vscode/index3.js
+++ .vscode/index3.js
@@ -11,12 +11,25 @@
 //  verifTableau("salut");
 
 // Ex2 Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
-function getFistElemArr(n){
-    if(Array.isArray(n)){
-        console.log(n[0]);
-    } else{
-        console.log("ceci n'est un tableau");
-    }
+// function getFistElemArr(n){
+//     if(Array.isArray(n)){
+//         console.log(n[0]);
+//     } else{
+//         console.log("ceci n'est un tableau");
+//     }
 
-}
-getFistElemArr(["Theo",5,6])
+// }
+getFistElemArr(["merci",3,5,6])
+// Ex3 Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
+function getFirstElements(array, n) {
+    if (n === undefined) {
+      return array[0];
+    } else {
+      return array.slice(0, n);
+    }
+  }
+  const myArray = [1, 2, 3, 4, 5];
+const firstElement = getFirstElements(myArray); // renvoie 1
+const firstTwoElements = getFirstElements(myArray, 2); // renvoie [1, 2]
+
+  
