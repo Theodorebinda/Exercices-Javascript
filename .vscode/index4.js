364b9f64777f6820f72b345f0ffb36bcc3dce529
Samba <theodorebinda@gmail.com>
2023-10-30 21:18:54 +0100

    program that displays a list of books you've read and those you want to read

--- .vscode/index4.js
+++ .vscode/index4.js
@@ -132,17 +132,60 @@
 // cat.pet();
 // cat.handelHappiness();
 // console.log(cat)
+
+
 // Ex6 The Recipe Card 
 // Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings). On separate lines (one console.log statement for each), log the recipe information so it looks like: Mole Serves: 2 Ingredients: cinnamon cumin cocoa
 // Création de l'objet recette
-let recette = {
-  titre: "Mole",
-  portions: 2,
-  ingredients: ["cannelle", "cumin", "cacao"]
-};
 
+// let recette = {
+//   titre: "Mole",
+//   portions: 2,
+//   ingredients: ["cannelle", "cumin", "cacao"]
+// };
 
-// Affichage des informations de la recette
-console.log(recette.titre);
-console.log("Portions : " + recette.portions);
-console.log("Ingrédients : " + recette.ingredients.join(" "));
+// // Affichage des informations de la recette
+// console.log(recette.titre);
+// console.log("Portions : " + recette.portions);
+// console.log("Ingrédients : " + recette.ingredients.join(" "));
+// Ex7 The Reading List
+// Keep track of which books you read and which books you want to read!
+// Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+// Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+// Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+const readingList = [
+  {
+    titre : "Seigneur des Anneaux",
+    auteur : "J.R.R. Tolkien",
+    dejaLu : true
+  },
+ {
+    titre : "pere Riche Pere Pauvre",
+    auteur : "R. Kiyosaki",
+    dejaLu : false
+  },
+  {
+    titre : "plus Malin Que Le Diable",
+    auteur : "Napolleon Hill",
+    dejaLu : true
+  },
+  {
+    titre : "Reflechissez Devenez Riche",
+    auteur : "Napolleon Hill",
+    dejaLu : true
+  }
+
+]
+readingList.forEach(element => {
+  livreLu = element.titre + " de" + " "+ element.auteur ;
+
+  if(readingList[3].dejaLu){
+    console.log(element + " Deja lu");
+  } else{
+    console.log(element + " Vous devez encore lire ");
+  }
+});
+
+// console.log(readingList);
+// console.log(readingList[1]);
