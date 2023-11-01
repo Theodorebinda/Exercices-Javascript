cdfc07e089eedcadc0c4994ed795454a15f98d9a
Samba <theodorebinda@gmail.com>
2023-11-01 21:35:36 +0100

    Ajout de l'exerc qui gere le logo

--- .vscode/dom.js
+++ .vscode/dom.js
@@ -10,14 +10,17 @@ const cinquiemeBloc = document.querySelector("#cinquiemeBloc");
 
 titre.innerHTML = "EXERCICES JAVASCRIPT";
 instruction.innerHTML = "Veillez Utiliser la console du navigateur pour voir les resultats associer aux Exercices";
-premierBloc.innerHTML = "Premiere Partie <br>index.js";
-deuxiemeBloc.innerHTML = "Deuxieme Parties <br> index.js";
+premierBloc.innerHTML = "Premiere Partie <br>variable.js";
+deuxiemeBloc.innerHTML = "Deuxieme Parties <br> condition.js";
 TroisiemeBloc.innerHTML = "Troisiemes Parties <br> sur index.js";
 quatriemeBloc.innerHTML = "Quatrieme  Parties <br> sur index.js";
 cinquiemeBloc.innerHTML = "Cinquieme  Parties <br> sur index.js";
 
 
-console.log(premierBloc);
+// console.log(premierBloc);
+
+// Ex1 You are given some names of different fruit and an unordered list html element; add a list item for each to the unordered list in page.
+// The Html sample code : <ul></ul>
 
 const fruitList = ["pomme", "banane", "tomate", "mangue"];
 const ul = document.querySelector("#list");
@@ -25,8 +28,32 @@ console.log(ul);
 
 fruitList.forEach(fruit => {
     const li = document.createElement("li")
-    li.setAttribute("title", "djo")
-    li.innerHTML = fruit
+li.innerHTML = fruit
     ul.appendChild(li)
-    console.log(li);
+    
+});
+
+// Ex2 Add the CroCoder logo (image element) as a child element to the existing div element.
+// The Html sample code : <div></div>
+// The uri : 
+// const imageSrc = "https://crocoder.dev/icon.png";
+
+const logo = document.querySelector("#logo");
+const imageSrc = "https://crocoder.dev/icon.png";
+const img = document.createElement("img");
+img.setAttribute("src", "" );
+img.src = imageSrc
+
+
+logo.appendChild(img)
+console.log(img)
+
+// EX3 Change the text in the first and the last list item element in every unordered list element on this page. Change it to 'first' and 'last'.
+
+const ulElement = document.querySelectorAll("ul");
+ulElement.forEach(function(element) {
+    const firstListItem = ulElement.querySelector('li:first-child');
+    const lastListItem = ulElement.querySelector('li:last-child');
+    
+    
 });
